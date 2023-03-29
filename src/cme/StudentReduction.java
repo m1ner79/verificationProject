f831b3b07c303ebde7560b461f5300b8daf57412
michal <michal.d.gornicki@gmail.com>
2023-03-29 14:53:28 +0100

    update decimal input and add rounding mode

--- src/cme/StudentReduction.java
+++ src/cme/StudentReduction.java
@@ -1,14 +1,16 @@
 package cme;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class StudentReduction implements Reduction {
     public BigDecimal calculateReduction(BigDecimal totalCost) {
-        BigDecimal threshold = new BigDecimal("5.50");
-        BigDecimal reductionRate = new BigDecimal("0.33");
+        BigDecimal threshold = new BigDecimal(5.5);
+        BigDecimal reductionRate = new BigDecimal(0.33);
         if (totalCost.compareTo(threshold) <= 0) {
             return totalCost;
         }
-        return threshold.add((totalCost.subtract(threshold)).multiply(BigDecimal.ONE.subtract(reductionRate)));
+        BigDecimal reducedCost = threshold.add((totalCost.subtract(threshold)).multiply(BigDecimal.ONE.subtract(reductionRate)));
+        return reducedCost.setScale(2, RoundingMode.CEILING);
     }
 }
